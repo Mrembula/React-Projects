a0d091f23775eac9659bdc71e2c7874a0b3419f3
Tebog <tebogosekhula@gmail.com>
2025-06-01 22:44:38 +0200

    Updated layout styling

--- React-complete-course/CRWN-CLOTHING/src/App.jsx
+++ React-complete-course/CRWN-CLOTHING/src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React from "react";
+import HomePage from "./components/page/homepage/homepage.component";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>Hello, world</h1>
+      <HomePage />
     </>
   );
 }
