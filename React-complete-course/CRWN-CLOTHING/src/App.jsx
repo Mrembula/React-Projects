c640bfb83481aa6824f0d234892534d8f1e743b2
Tebog <tebogosekhula@gmail.com>
2025-06-05 17:08:24 +0200

    update our menu-item to navigate withRouter HOC

--- React-complete-course/CRWN-CLOTHING/src/App.jsx
+++ React-complete-course/CRWN-CLOTHING/src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import HomePage from "./components/page/homepage/homepage.component";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+      </Switch>
     </>
   );
 }
